68ebb5f89f40e2f0e05505921376da783e3c93f7
Stanislav <cjp363879>
2015-03-01 11:40:18 +0300

    add get and set

--- src/HiVarCache.php
+++ src/HiVarCache.php
@@ -1,69 +1,37 @@
 <?php
-/**
- * Created for the project "hivarCache"
- *
- * @author: Stanislav Semenov (CJP2600)
- * @email: cjp2600@ya.ru
- *
- * @date: 27.02.2015
- * @time: 17:53
- */
-
 
 /**
  * Class HiVarCache
  */
-class HiVarCache
-{
-    /**
-     * @var
-     */
-    protected static $instance;
-    /**
-     * @var array
-     */
-    protected static $data = array();
+class HiVarCache {
 
     /**
-     *
+     * Cached objects
+     * @static array
      */
-    protected function __construct()
-    {
-    }
-
-    /**
-     * getInstance
-     * @return HiVarCache
-     */
-    public static function getInstance()
-    {
-        if (!self::$instance) {
-            self::$instance = new self();
-        }
-
-        return self::$instance;
-    }
-
-    /**
-     * get
-     * @param $key
-     * @return null
-     */
-    public function get($key)
-    {
-        self::getInstance();
-
-        return isset(self::$data[$key]) ? self::$data[$key] : null;
-    }
+    private static $cache = array();
 
     /**
      * set
      * @param $key
      * @param $value
      */
-    public function set($key, $value)
+    public static function set($key,$value)
     {
-        self::getInstance();
-        self::$data[$key] = $value;
+        static::$cache[$key] = $value;
     }
+
+    /**
+     * Get the cached value
+     * @param $key
+     * @return mixed Cached value
+     */
+    public static function get($key)
+    {
+        if (  isset(static::$cache[$key] ) ) {
+            return static::$cache[$key];
+        }
+        return false;
+    }
+
 }
